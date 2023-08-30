d189eb4e95a5a3d0e287d5f5decca8b4d2960e7d
Vixi <victoriaaskander@gmail.com>
2023-08-30 18:45:31 +0200

    Test V

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
+import './App.css';
 
 // Components
-import Navbar from './Navbar.jsx'
+import Navbar from './Navbar.jsx';
 
 // Pages
 import Boende from './pages/Boende.jsx';
@@ -12,22 +12,22 @@ import Oppettider from './pages/Oppettider.jsx';
 import Kontakt from './pages/Kontakt.jsx';
 import Home from './pages/Hem.jsx';
 
-
 function App() {
   return (
     <div className='App'>
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/boende" element={<Boende />} />
-                <Route path="/aktiviteter" element={<Aktiviteter />} />
-                <Route path="/oppettider" element={<Oppettider />} />
-                <Route path="/kontakt" element={<Kontakt />} />
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/boende' element={<Boende />} />
+          <h1>Extra Test</h1>
+          <Route path='/aktiviteter' element={<Aktiviteter />} />
+          <Route path='/oppettider' element={<Oppettider />} />
+          <Route path='/kontakt' element={<Kontakt />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
-);
+  );
 }
 
 export default App;
